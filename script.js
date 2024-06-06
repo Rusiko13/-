fa4ebabbff3208c456a22311b91027e7ff28e8a3
Rusiko13 <rusochlaidze@gmail.com>
2024-06-07 01:32:37 +0400

    need to be reviewed

--- script.js
+++ script.js
@@ -192,5 +192,25 @@ window.addEventListener('scroll', function() {
     }
   });
 
+  //burger menu
+  function burgerMenu() {
+    let burgerMenu = document.querySelector(".burger-bar");
+    let navigation = document.querySelector(".navigation");
+  
+    burgerMenu.addEventListener("click", () => {
+      burgerMenu.classList.toggle("active");
+      navigation.classList.toggle("active");
+    });
+    console.log("hello world");
+}
+let navigation = document.getElementById("navEl");
+let burger = document.getElementById("burgerBtn");
+
+burger.addEventListener("click", function () {
+
+  navigation.classList.toggle("navigation");
+  navigation.classList.toggle("visible");
+});
+
 
 
